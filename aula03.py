918209110d24d319ebb916d60d14a0ba80a98ce8
Cinthya Taniguchi <cinthyat@gmail.com>
2025-11-21 12:37:39 +0100

    correções iniciais

--- aula03.py
+++ aula03.py
@@ -1,17 +1,20 @@
 import sqlite3
 from random import randint
 
-conexao = sqlite3.connect("Banco de Dados")
+conexao = sqlite3.connect("banco_de_dados.db")
 cursor = conexao.cursor()
 
-#cursor.execute('CREATE TABLE minha_tabela(Data text, Nome text, Idade real )')
-#conexao.commit()
+cursor.execute('CREATE TABLE IF NOT EXISTS minha_tabela(Data text, Nome text, Idade real )')
 
-#cursor.execute('INSERT INTO minha_tabela VALUES ("01/01/2021", "Odemir", 30)')
-#cursor.execute('INSERT INTO minha_tabela VALUES ("05/01/2021", "Lucas", 30)')
+cursor.execute('INSERT INTO minha_tabela VALUES ("01/01/2021", "Odemir", 30)')
+cursor.execute('INSERT INTO minha_tabela VALUES ("05/01/2021", "Lucas", 30)')
 
-'''for loop in range(10):
+for _ in range(10):
     numero = randint(10,20)
-    cursor.execute(f'INSERT INTO minha_tabela VALUES ("10/01/2021", "Lucas", {numero})')'''
+    cursor.execute(f'INSERT INTO minha_tabela VALUES ("10/01/2021", "Lucas", {numero})')
 
 
+consulta = cursor.execute("SELECT * FROM minha_tabela").fetchall()
+print(consulta)
+
+conexao.commit()
