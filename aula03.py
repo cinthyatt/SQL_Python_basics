359d743059802570c22bf407b8c97e2ebc032d2b
Cinthya Taniguchi <cinthyat@gmail.com>
2025-11-21 12:54:59 +0100

    aula06e07

--- aula03.py
+++ aula03.py
@@ -4,7 +4,7 @@ from random import randint
 conexao = sqlite3.connect("banco_de_dados.db")
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE IF NOT EXISTS minha_tabela(Data text, Nome text, Idade real )')
+'''cursor.execute('CREATE TABLE IF NOT EXISTS minha_tabela(Data text, Nome text, Idade real )')
 
 cursor.execute('INSERT INTO minha_tabela VALUES ("01/01/2021", "Odemir", 30)')
 cursor.execute('INSERT INTO minha_tabela VALUES ("05/01/2021", "Lucas", 30)')
@@ -15,6 +15,10 @@ for _ in range(10):
 
 
 consulta = cursor.execute("SELECT * FROM minha_tabela").fetchall()
-print(consulta)
+for linha in consulta:
+    print(linha)
 
-conexao.commit()
+conexao.commit()'''
+
+consulta = cursor.execute("SELECT * FROM minha_tabela WHERE Nome='Odemir'").fetchall()
+print(consulta)
